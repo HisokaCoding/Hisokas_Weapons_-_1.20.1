1c1ffaee0f90cfa0e35b08b4403878f2e21f4492
HisokaCoding <zarniacci337@gmail.com>
2024-03-17 19:21:48 +0500

    Initial commit

---
+++ src/main/java/net/hisoka/hisokasweapons/HisokasWeapons.java
@@ -0,0 +1,16 @@
+package net.hisoka.hisokasweapons;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HisokasWeapons implements ModInitializer {
+	public static final String MOD_ID = "hisokasweapons";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hisoka/hisokasweapons/HisokasWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.hisoka.hisokasweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HisokasWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
